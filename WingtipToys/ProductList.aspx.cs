11bd1b51aebc6689f2228b308bf357d3ca7cbe0f
ABDEL <ABDEL@outlook.fr>
2022-02-23 11:26:54 +0100

    Add navigation pages

--- WingtipToys/ProductList.aspx.cs
+++ WingtipToys/ProductList.aspx.cs
@@ -2,23 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WingtipToys.Models;
 
 namespace WingtipToys
 {
-    public partial class SiteMaster : MasterPage
+    public partial class ProductList : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
 
-        public IQueryable<Category> GetCategories()
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
         {
             var _db = new WingtipToys.Models.ProductContext();
-            IQueryable<Category> query = _db.Categories;
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
             return query;
         }
     }
