3e34d0ec4dd1947b902afa9ae146c49c1f02cf30
ABDEL <ABDEL@outlook.fr>
2022-02-28 17:09:07 +0100

    Add some files to project

--- WingtipToys/Models/ProductContext.cs
+++ WingtipToys/Models/ProductContext.cs
@@ -13,6 +13,10 @@ namespace WingtipToys.Models
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
+
+        
     }
 }
 
